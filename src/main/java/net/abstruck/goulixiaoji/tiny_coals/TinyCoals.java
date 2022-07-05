050c3920cf6999427c386533983fcc07fdf46596
Goulixiaoji <84974878+Goulixiaoji@users.noreply.github.com>
2022-07-05 11:52:52 +0800

    Add files via upload

--- src/main/java/net/abstruck/goulixiaoji/tiny_coals/TinyCoals.java
+++ src/main/java/net/abstruck/goulixiaoji/tiny_coals/TinyCoals.java
@@ -8,6 +8,9 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+/**
+ * @author Goulixiaoji
+ */
 @Mod(TinyCoals.MOD_ID)
 public class TinyCoals {
     private static final Logger LOGGER = LogManager.getLogger();
--- src/main/java/net/abstruck/goulixiaoji/tiny_coals/registry/CoalRegistry.java
+++ src/main/java/net/abstruck/goulixiaoji/tiny_coals/registry/CoalRegistry.java
@@ -2,11 +2,14 @@ package net.abstruck.goulixiaoji.tiny_coals.registry;
 
 import net.abstruck.goulixiaoji.tiny_coals.TinyCoals;
 import net.abstruck.goulixiaoji.tiny_coals.common.item.Coals;
-import net.minecraft.item.Item;
-import net.minecraftforge.fml.RegistryObject;
+import net.minecraft.world.item.Item;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
+/**
+ * @author Goulixiaoji
+ */
 public class CoalRegistry {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, TinyCoals.MOD_ID);
 
